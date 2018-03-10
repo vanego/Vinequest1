2c7f336d1f029215a06075fcd13524df2a37ef6a
Unknown <vanegogogo@gmail.com>
2018-03-10 10:47:48 -0800

    working search bar

--- assets/scripts/search.js
+++ assets/scripts/search.js
@@ -12,14 +12,14 @@ $(document).ready(function() {
 
       var userInput= $(".input-field").val();
 
-      // API key
-      var queryURL = "https://api.unsplash.com/search/photos?client_id=81749fea1cd0e85289f700fd98bd3488b5b786cfb861291a86beaf4230d2866c&page=1&query=wine";
+      // // API key
+      // var queryURL = "https://api.unsplash.com/search/photos?client_id=81749fea1cd0e85289f700fd98bd3488b5b786cfb861291a86beaf4230d2866c&page=1&query=wine";
       
       if (userInput.length > 0) {
       	// use user input to search instagram api
       	console.log(userInput);
       	
-      	function
+      	// function
 
 
       	// clear input field
